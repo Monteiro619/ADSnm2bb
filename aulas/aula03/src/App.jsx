0ecf1feca5f81982ce05aebe66a237678784ac1d
unknown <2324290080@ACADEMICO.IESB.BR>
2024-08-29 21:51:37 -0300

    codigo da aula 3

---
+++ aulas/aula03/src/App.jsx
@@ -0,0 +1,14 @@
+// Conteudo da pagina
+
+import Cabecalho from "./components/Cabecalho";
+
+function App(){
+  return ( 
+  <>
+    <Cabecalho />
+    <p>Ola</p>
+  </>
+  );
+}
+
+export default App;
