a33c241ad1cceab704aa63526cf35caf0dc11dca
unknown <2324290080@ACADEMICO.IESB.BR>
2024-09-30 21:50:05 -0300

    incluindo aula07

---
+++ aulas/aula07/src/components/InputNome.jsx
@@ -0,0 +1,19 @@
+function InputNome (props) {
+    const regras = {
+        required: {value: true, message: "Nome é obrigatorio"},
+        maxLength: {
+            value: 100,
+            message: "O nome deve ter no minimo 100 caracteres"
+        },
+    };
+
+    return (
+        <>
+            <label htmlFor="nome">Nome</label>
+            <input type="text" {...props.register("nome", regras)} />
+            {props.error && <p>{props.error.message}</p>}
+        </>
+    );
+}
+
+export default InputNome;
