a8e2b566dd9960103189bca3c04537bbab3282ac
unknown <2324290080@ACADEMICO.IESB.BR>
2024-11-07 21:53:10 -0300

    aula 11.1

--- aulas/aula11/front/src/pages/Home.jsx
+++ aulas/aula11/front/src/pages/Home.jsx
@@ -1,8 +1,22 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+
 function Home () {
+    const navigate = useNavigate();
+
+    const { logout } = useContext(AuthContext);
+
+    const onSair = () => {
+        logout();
+        navigate("/login");
+    }
+
+
     return (
         <>
             <h1>Home</h1>
-            <button>Sair</button>
+            <button onClick={onSair}>Sair</button>
         </>
     );
 }
